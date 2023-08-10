cb49b07f348fbf150e791d1d3ca8668b34baf6b2
Nata3951 <Nata3951@gmail.com>
2023-08-10 22:38:28 +0300

    Update 2308 leetcode.js

--- 2308 leetcode.js
+++ 2308 leetcode.js
@@ -1,4 +1,8 @@
-var findDisappearedNumbers = function(nums) {
+// let nums = [1, 1, 2, 4];
+// let nums = [4, 3, 2, 7, 8, 2, 3, 1];
+let nums = [1, 1];
+
+var findDisappearedNumbers = function (nums) {
     nums[nums.length] = nums[0];
     nums[0] = 0;
     let hold = [0];
@@ -7,16 +11,20 @@ var findDisappearedNumbers = function(nums) {
         while (!hold.includes(k)) {
             hold.push(k);
             k = nums[k]; // next index  
-        };
-        hold.forEach(el => nums[el] = 0)
+        }
+        for (let j = 0; j < hold.length; j++) {
+            nums[hold[j]] = 0;
+        }
     }
     
     nums.forEach((el, ind) => {
         if (el > 0) nums[ind] = ind;
-    })
+    });
 
     nums.sort((a, b) => b - a);
     let first_zero = nums.indexOf(0);
     nums.splice(first_zero);
     return nums;
 };
+
+console.log(findDisappearedNumbers(nums));
